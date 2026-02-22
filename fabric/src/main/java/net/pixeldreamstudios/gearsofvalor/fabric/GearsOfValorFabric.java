ecf62b13a69ad79cb16f20bd432d30c73965efa9
Bandit Bytes <banditheeler2020@gmail.com>
2026-02-22 13:26:45 -0500

    fix fabric ores

--- fabric/src/main/java/net/pixeldreamstudios/gearsofvalor/fabric/GearsOfValorFabric.java
+++ fabric/src/main/java/net/pixeldreamstudios/gearsofvalor/fabric/GearsOfValorFabric.java
@@ -10,5 +10,7 @@ public final class GearsOfValorFabric implements ModInitializer {
     public void onInitialize() {
         GearsOfValorMod.init();
         GearsOfValorMod.initAzIdentityRegistry();
+
+        FabricWorldgen.init();
     }
 }
